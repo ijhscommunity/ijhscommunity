4921aa5eae73f926909cc8d7e01144cc30f4f29d
Zeyu Yao <novodoodle@gmail.com>
2023-02-26 12:16:42 +0800

    Add metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,11 @@ export const metadata = {
         default: "International Junior Honor Society",
         template: "%s | International Junior Honor Society",
     },
+    description: {
+        default:
+            "The International Junior Honor Society is an honor society that focuses on developing leaders and enabling its members to achieve success in academic and life pursuits.",
+        template: "%s",
+    },
     openGraph: {
         title: "International Junior Honor Society",
         description: {
@@ -28,6 +33,41 @@ export const metadata = {
         locale: "en-US",
         type: "website",
     },
+    twitter: {
+        card: "summary_large_image",
+        title: "International Junior Honor Society",
+        description: {
+            default:
+                "The International Junior Honor Society is an honor society that focuses on developing leaders and enabling its members to achieve success in academic and life pursuits.",
+            template: "%s",
+        },
+        creator: "@ijhscommunity",
+        siteId: "1614121836564066305",
+        creatorId: "1614121836564066305",
+        images: [
+            {
+                url: "https://ijhscommunity.vercel.app/og-image.png",
+                width: 1200,
+                height: 630,
+                alt: "International Junior Honor Society",
+            },
+        ],
+    },
+    icons: {
+        apple: "/apple-touch-icon.png",
+        icon: ["/favicon-32x32.png", "/favicon-16x16.png"],
+        other: [
+            {
+                rel: "android-chrome-192x192",
+                href: "/android-chrome-192x192.png",
+            },
+            {
+                rel: "android-chrome-512x512",
+                href: "/android-chrome-512x512.png",
+            },
+        ],
+    },
+    manifest: "/site.webmanifest",
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,17 @@
 // import Image from 'next/image'
-import styles from './page.module.css'
+import styles from "./page.module.css";
 
 export const metadata = {
-  description: "Cool stuff coming soon!",
-  openGraph: {
     description: "Cool stuff coming soon!",
-  },
+    openGraph: {
+        description: "Cool stuff coming soon!",
+    },
 };
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <h1 className={styles.title}>
-        Cool stuff coming soon!
-      </h1>
-    </main>
-  )
+    return (
+        <main className={styles.main}>
+            <h1 className={styles.title}>Cool stuff coming soon!</h1>
+        </main>
+    );
 }
