02e6db159ab734c07a0e2bf90f146cb2905bf6e0
Zeyu Yao <novodoodle@gmail.com>
2023-02-26 12:30:30 +0800

    Metadata edits (#1)
    * Update image dir
    * Remove templates for now

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,54 +4,31 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-    title: {
-        default: "International Junior Honor Society",
-        template: "%s | International Junior Honor Society",
-    },
-    description: {
-        default:
-            "The International Junior Honor Society is an honor society that focuses on developing leaders and enabling its members to achieve success in academic and life pursuits.",
-        template: "%s",
-    },
+    title: "International Junior Honor Society",
+    description: "The International Junior Honor Society is an honor society that focuses on developing leaders and enabling its members to achieve success in academic and life pursuits.",
     openGraph: {
         title: "International Junior Honor Society",
-        description: {
-            default:
-                "The International Junior Honor Society is an honor society that focuses on developing leaders and enabling its members to achieve success in academic and life pursuits.",
-            template: "%s",
-        },
+        description: "The International Junior Honor Society is an honor society that focuses on developing leaders and enabling its members to achieve success in academic and life pursuits.",
         url: "https://ijhscommunity.vercel.app/",
         siteName: "International Junior Honor Society",
-        images: [
-            {
-                url: "https://ijhscommunity.vercel.app/og-image.png",
-                width: 1200,
-                height: 630,
-                alt: "International Junior Honor Society",
-            },
-        ],
+        images: {
+            url: "https://ijhscommunity.vercel.app/og-image.png",
+            alt: "International Junior Honor Society",
+        },
         locale: "en-US",
         type: "website",
     },
     twitter: {
         card: "summary_large_image",
         title: "International Junior Honor Society",
-        description: {
-            default:
-                "The International Junior Honor Society is an honor society that focuses on developing leaders and enabling its members to achieve success in academic and life pursuits.",
-            template: "%s",
-        },
+        description: "The International Junior Honor Society is an honor society that focuses on developing leaders and enabling its members to achieve success in academic and life pursuits.",
         creator: "@ijhscommunity",
         siteId: "1614121836564066305",
         creatorId: "1614121836564066305",
-        images: [
-            {
-                url: "https://ijhscommunity.vercel.app/og-image.png",
-                width: 1200,
-                height: 630,
-                alt: "International Junior Honor Society",
-            },
-        ],
+        images: {
+            url: "https://ijhscommunity.vercel.app/og-image.png",
+            alt: "International Junior Honor Society",
+        },
     },
     icons: {
         apple: "/apple-touch-icon.png",
